321ef257f4cc872e2a8293f0547eb8ed34ce0256
Jonas Bieri <jonas.bieri@gmail.com>
2019-03-13 10:37:50 +0000

    Increase tooltip size to make room for all text in Firefox on Windows

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quartierindex/precache-manifest.5276a63392c5c73c4323bafab9b853a7.js"
+  "/quartierindex/precache-manifest.0e76c61e850fb3d7785bf1437876f911.js"
 );
 
 workbox.clientsClaim();
